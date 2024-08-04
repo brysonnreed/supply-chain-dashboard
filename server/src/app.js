ecaf42dece327b667c471b13417458b49c3d968f
Bryson Reed <brysonjreed@gmail.com>
2024-08-04 00:47:26 -0500

    Bug Fix

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const PORT = process.env.PORT || 5000; // Corrected to 'PORT'
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("API is running...");
+	// res.sendFile(path.join(__dirname, "../../client/build/index.html"), function (err) {
+	// 	if (err) {
+	// 		res.status(500).send(err);
+	// 	}
+	// });
 });
 
-module.exports = app;
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
--- server/src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const PORT = process.env.port || 5000;
-
-app.listen(PORT, () => {
-	console.log(`Server Running on Port ${PORT}`);
-});
