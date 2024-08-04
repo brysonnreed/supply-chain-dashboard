6662dc332991ace3865008331111e5b4cbe43f74
Bryson Reed <brysonjreed@gmail.com>
2024-08-04 01:17:57 -0500

    Update build directory

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Corrected to 'PORT'
@@ -8,17 +7,13 @@ const PORT = process.env.PORT || 5000; // Corrected to 'PORT'
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// API endpoint example
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
 	res.send("API is running...");
-});
-
-// Handle any other routes (i.e., serve the React app)
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	res.sendFile(path.join(__dirname, "../../client/build/index.html"), function (err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
 });
 
 // Start the server
